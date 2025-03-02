3f034b7162b6cf9e90705ef3efcf2e11cfd05e5e
Franz090 <francisoblepias7@gmail.com>
2025-03-02 23:40:28 +0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import InterviewAssistant from "./components/InterviewAssistant";
+import { Suspense, lazy } from "react";
+
+const InterviewAssistant = lazy(() => import("./components/InterviewAssistant"));
 
 function App() {
   return (
-    <div className="p-5">
-      <InterviewAssistant />
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center">
+            <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        }
+      >
+        <InterviewAssistant />
+      </Suspense>
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {port:3000},
+  server: {
+    port: 3000
+  },
   esbuild: {
     jsxInject: `import "regenerator-runtime/runtime";`,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Prevents chunk warning
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves node_modules into a separate chunk
+          }
+        },
+      },
+    },
   },
 })
